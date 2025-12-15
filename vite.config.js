af713256d5b4a73a5963c6a29be53de60acc4195
useless <gocamper03@gmail.com>
2025-12-15 17:52:44 +0300

    Setup pages deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+  base: "https://github.com/useless-m8/test",
   plugins: [
     react({
       babel: {
